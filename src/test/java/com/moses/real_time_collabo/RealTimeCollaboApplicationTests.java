f6434d3e21aa17d1b0ccfb00aaf492956e706212
mwayandau1 <mosesayandau1@gmail.com>
2024-12-24 09:29:06 +0000

    first commit

---
+++ src/test/java/com/moses/real_time_collabo/RealTimeCollaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moses.real_time_collabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeCollaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
